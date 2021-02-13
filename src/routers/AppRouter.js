5a8979c9d3db9a415e3c1bed63ea661e8a312181
CarlosGRJ <carlosgrj@outlook.com>
2021-02-13 14:19:38 -0600

    Protección de rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path='/login' component={LoginScreen} />
+          <PublicRoute
+            exact
+            path='/login'
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
 
-          <Route path='/' component={DashboardRoutes} />
+          <PrivateRoute
+            path='/'
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
